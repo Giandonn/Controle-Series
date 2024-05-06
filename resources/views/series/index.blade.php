cf183a24162c65a0ef61952b28c6ef421f69f43f
Giandonn <lucas_raineri04@hotmail.com>
2024-05-06 16:43:16 -0300

    new view to create new series

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,4 +1,6 @@
 <x-layout title='Séries'>
+    <a href="/series/criar">Adicionar</a>
+
     <ul>
         @foreach($series as $s)
         <li>{{ $s }}</li>
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ Route::get('/', function () {
 });
 
 Route::get('/series', [App\Http\Controllers\SeriesController::class, 'index']);
+
+Route::get('/series/criar', [App\Http\Controllers\SeriesController::class, 'create']);
