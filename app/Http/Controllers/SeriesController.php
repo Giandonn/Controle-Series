3c3c7314ab26dbad0c41ffbc683a92efa2d648f7
Giandonn <lucas_raineri04@hotmail.com>
2024-05-06 16:20:36 -0300

    upt

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -13,6 +13,6 @@ class SeriesController extends Controller
             'tartarugas ninja'
         ];
 
-        return view('listar-series')->with('series', $series);
+        return view('series.index')->with('series', $series);
     }
 }
